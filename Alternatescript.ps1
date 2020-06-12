4dae0db677411c65febfc7d06c2fc44951a185de
CyberSauce <wsgoth@gmail.com>
2020-06-11 21:32:20 -0500

    Add files via upload

---
+++ Alternatescript.ps1
@@ -0,0 +1,14 @@
+Get-ChildItem "$env:USERPROFILE\AppData\Roaming\Microsoft\Teams\Backgrounds\Uploads\" 
+Invoke-WebRequest -Uri "https://i.imgur.com/string.png" -Outfile "$env:USERPROFILE\AppData\Roaming\Microsoft\Teams\Backgrounds\Uploads\pic1.png"
+
+Get-ChildItem "$env:USERPROFILE\AppData\Roaming\Microsoft\Teams\Backgrounds\Uploads\" 
+Invoke-WebRequest -Uri "https://i.imgur.com/string.png" -Outfile "$env:USERPROFILE\AppData\Roaming\Microsoft\Teams\Backgrounds\Uploads\pic2.png"
+
+Get-ChildItem "$env:USERPROFILE\AppData\Roaming\Microsoft\Teams\Backgrounds\Uploads\" 
+Invoke-WebRequest -Uri "https://i.imgur.com/string.png" -Outfile "$env:USERPROFILE\AppData\Roaming\Microsoft\Teams\Backgrounds\Uploads\pic3.png"
+
+Get-ChildItem "$env:USERPROFILE\AppData\Roaming\Microsoft\Teams\Backgrounds\Uploads\" 
+Invoke-WebRequest -Uri "https://i.imgur.com/string.png" -Outfile "$env:USERPROFILE\AppData\Roaming\Microsoft\Teams\Backgrounds\Uploads\pic4.png"
+
+Get-ChildItem "$env:USERPROFILE\AppData\Roaming\Microsoft\Teams\Backgrounds\Uploads\" 
+Invoke-WebRequest -Uri "https://i.imgur.com/string.png" -Outfile "$env:USERPROFILE\AppData\Roaming\Microsoft\Teams\Backgrounds\Uploads\pic5.png"
